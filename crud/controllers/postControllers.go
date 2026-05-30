a370bc4484f859f40caac2d7257d9f594a8cc3e0
Ayyadurai <kmayyadurai286@gmail.com>
2026-05-30 22:06:43 +0530

    feat: enhance post management with update and delete functionalities

--- crud/controllers/postControllers.go
+++ crud/controllers/postControllers.go
@@ -8,46 +8,90 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func PostCreate(c *gin.Context){
-	var body struct{
+func PostCreate(c *gin.Context) {
+	var body struct {
 		Title string
-		Body string
+		Body  string
 	}
 	c.Bind(&body)
 
 	post := models.Post{Title: body.Title, Body: body.Body}
 
-	result := initializers.DB.Create(&post ) // pass pointer of data to Create
+	result := initializers.DB.Create(&post) // pass pointer of data to Create
 
 	if result.Error != nil {
 		c.Status(400)
 		return
 	}
 
-	c.JSON(200,gin.H{
-		"result":"Post created",
-		"postId":post.ID,
+	c.JSON(200, gin.H{
+		"result": "Post created",
+		"postId": post.ID,
 	})
 }
 
+func PostList(c *gin.Context) {
+	var posts []models.Post
 
-func PostDetail(c *gin.Context){
+	initializers.DB.Find(&posts)
+
+	c.JSON(http.StatusOK, gin.H{
+		"posts": posts,
+	})
+}
+
+func PostDetail(c *gin.Context) {
 	var post models.Post
 
-	id := c.Param("id") 
+	id := c.Param("id")
 
 	result := initializers.DB.First(&post, id)
 
 	if result.Error != nil {
-		c.JSON(http.StatusBadRequest,gin.H{
-			"post":post,
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Post not found",
 		})
+		return
 	}
 
-	c.JSON(http.StatusOK,gin.H{
-		"status":"OK",
-		"post" : post,
+	c.JSON(http.StatusOK, gin.H{
+		"status": "OK",
+		"post":   post,
 	})
 
+}
+
+func PostUpdate(c *gin.Context) {
+	id := c.Param("id")
+
+	var body struct {
+		Title string
+		Body  string
+	}
+	c.Bind(&body)
+
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title, Body: body.Body,
+	})
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "OK",
+		"post":   post,
+	})
+
+}
+
+func PostDelete(c *gin.Context) {
+
+	id := c.Param("id")
+
+	initializers.DB.Delete(&models.Post{}, id)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "OK",
+	})
 
 }
--- crud/main.go
+++ crud/main.go
@@ -12,11 +12,14 @@ func init(){
     initializers.LoadEnvVariables()
     initializers.ConnectDB()
 }
-
+ 
 func main() {
   router := gin.Default()
   router.POST("/posts",controllers.PostCreate)
+  router.GET("/posts",controllers.PostList)
   router.GET("/posts/:id",controllers.PostDetail)
+  router.PUT("/posts/:id",controllers.PostUpdate)
+  router.DELETE("/posts/:id",controllers.PostDelete)
 
   router.Run() // listens on 8080 by default
 }
