d44112fd9dc6e8351afacbf226f7d687c10cf1f7
Ayyadurai <kmayyadurai286@gmail.com>
2026-06-06 22:27:36 +0530

    feat: implement basic channel usage for concurrent number generation

---
+++ concurrency/chanbasics/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// func main(){
+// 	ch := make(chan int)
+
+// 	go func(){
+// 		ch <- 42
+// 	}()
+
+// 	x := <- ch
+// 	fmt.Println("Got : ",x)
+// }
+
+
+
+func main(){
+	ch := make(chan int)
+	go func ()  {
+		for i:=1;i<=5;i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	for n:= range ch{
+		fmt.Println("got :",n)
+	}
+
+}
