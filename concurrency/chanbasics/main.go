c70d6d47d459339a39e46fc327f5cad04eebb429
Ayyadurai <kmayyadurai286@gmail.com>
2026-06-07 09:38:22 +0530

    feat: update main function to demonstrate string channel usage with goroutines

--- concurrency/chanbasics/main.go
+++ concurrency/chanbasics/main.go
@@ -15,17 +15,29 @@ import "fmt"
 
 
 
-func main(){
-	ch := make(chan int)
-	go func ()  {
-		for i:=1;i<=5;i++ {
-			ch <- i
-		}
-		close(ch)
-	}()
+// func main(){
+// 	ch := make(chan int)
+// 	go func ()  {
+// 		for i:=1;i<=5;i++ {
+// 			ch <- i
+// 		}
+// 		close(ch)
+// 	}()
 
-	for n:= range ch{
-		fmt.Println("got :",n)
-	}
+// 	for n:= range ch{
+// 		fmt.Println("got :",n)
+// 	}
+
+// }
+
+
+func main(){
+	ch := make(chan string)
+
+	go func(){ ch <- "hello from goroutine A" }()
+	go func(){ ch <- "hello from goroutine B" }()
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 
 }
